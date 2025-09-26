a1c1ad5634a69312cf9eefcfebaf67a840d85f76
Blink <137730237+RoyalBounce@users.noreply.github.com>
2025-09-25 23:14:15 -0300

    Update exp.rb

--- lib/math-utils/exp.rb
+++ lib/math-utils/exp.rb
@@ -1,1 +1,19 @@
+module MathUtils
+  module Utils
+    def self.fibonacci(n)
+      return 0 if n == 0
+      return 1 if n == 1
 
+      a = 0
+      b = 1
+
+      (2..n).each do
+        next = a + b
+        a = b
+        b = next
+      end
+
+      return b
+    end
+  end
+end
